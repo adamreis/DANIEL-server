3e4c0aaeb088d2ea0444eb0314f83e7ca65c279a
Daniel Moreh <dmoreh@gmail.com>
2014-12-30 14:43:37 +0200

    set up basic flask server

---
+++ app/views.py
@@ -0,0 +1,22 @@
+from app import app
+from flask import Flask, request, jsonify
+import kairos
+
+DEFAULT_GALLERY = 'default_gallery'
+
+# App Logic
+@app.route('/', methods=['GET'])
+def index():
+    return 'yo'
+
+@app.route('/upload/<name>', methods=['POST'])
+def upload(name):
+    img_url = request.form['img_url']
+    success = kairos.add_face_url(img_url, name, DEFAULT_GALLERY)
+    return jsonify({'success': success})
+
+@app.route('/verify', methods=['GET'])
+def verify():
+    link = request.args.get('img_url')
+    allowed = kairos.check_face_url(img_url, DEFAULT_GALLERY)
+    return jsonify({'allowed': allowed})
