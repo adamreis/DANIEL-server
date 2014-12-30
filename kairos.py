34493f6de8e2218b326acb121269ebfd95d029ef
Adam Reis <adamhreis@gmail.com>
2014-12-30 14:47:38 +0200

    now returns name of user on success

--- kairos.py
+++ kairos.py
@@ -17,6 +17,7 @@ def kairos_post(endpoint, data):
     return r
 
 def add_face_file(base64_img, name, gallery):
+    """ returns True on success, False on failure """
     data = {"image" : base64_img,
             "subject_id" : name,
             "gallery_name" : gallery}
@@ -27,6 +28,7 @@ def add_face_file(base64_img, name, gallery):
     return 'Errors' not in r
 
 def add_face_url(img_url, name, gallery):
+    """ returns True on success, False on failure """
     data = {"url" : img_url,
             "subject_id" : name,
             "gallery_name" : gallery}
@@ -37,28 +39,36 @@ def add_face_url(img_url, name, gallery):
     return 'Errors' not in r
 
 def check_face_file(base64_img, gallery):
+    """ returns name of person (string) on success,
+        None on failure """
     data = {"image" : base64_img,
             "threshold" : 0.7,
-            "gallery_name" : "test"}
+            "gallery_name" : gallery}
 
     r = kairos_post("recognize", data).json()
 
-    # returns true if this succeeds
-    return r.get('images')[0].get('transaction').get('status') == 'success'
+    if r.get('images')[0].get('transaction').get('status') != 'success':
+        return None
+    else:
+        return r.get('images')[0].get('transaction').get('subject')
 
 def check_face_url(img_url, gallery):
+    """ returns name of person (string) on success,
+        None on failure """
+
     data = {"url" : img_url,
             "threshold" : 0.7,
-            "gallery_name" : "test"}
+            "gallery_name" : gallery}
 
     r = kairos_post("recognize", data).json()
 
-    # returns true if this succeeds
-    return r.get('images')[0].get('transaction').get('status') == 'success'
-
+    if r.get('images')[0].get('transaction').get('status') != 'success':
+        return None
+    else:
+        return r.get('images')[0].get('transaction').get('subject')
 
 # test enroll
 # r = add_face_file(base64_img('test_pix/daniel6.jpg'), 'danielm', 'test')
 
 # test recognize
-# r = check_face_file(base64_img('test_pix/daniel6.jpg'), 'test')
+# r = check_face_file(base64_img('test_pix/adam3.jpg'), 'test')
