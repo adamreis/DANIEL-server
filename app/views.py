c240fe861ede82272184c9d6b5b6a662cd507426
Daniel Moreh <dmoreh@gmail.com>
2014-12-30 15:40:49 +0200

    use request data instead of path params

--- app/views.py
+++ app/views.py
@@ -9,15 +9,18 @@ DEFAULT_GALLERY = 'default_gallery'
 def index():
     return 'yo'
 
-@app.route('/upload/<name>', methods=['POST'])
+@app.route('/upload', methods=['POST'])
 def upload(name):
-    img_url = request.form['img_url']
+    img_url = request.data['img_url']
+    name = request.data['name']
+
     success = kairos.add_face_url(img_url, name, DEFAULT_GALLERY)
     return jsonify({'success': success})
 
 @app.route('/verify', methods=['GET'])
 def verify():
-    link = request.args.get('img_url')
+    img_url = request.args.get('img_url')
+
     name = kairos.identify_face_url(img_url, DEFAULT_GALLERY)
     allowed = name is not None
     # TODO: open the door.
